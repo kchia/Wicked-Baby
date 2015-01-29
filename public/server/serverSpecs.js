3429827b88790f9a26f1df5d64fe3320d086fce2
Hou Chia <houchia@Hous-MacBook-Pro.local>
2015-01-28 17:07:40 -0800

    (test) sets up test files for server

---
+++ public/server/serverSpecs.js
@@ -0,0 +1,37 @@
+describe('server',function(){
+
+  describe('Thumb Clicks History: ', function(){
+  	it('should return the total number of clicks for each student', function(){});
+  });
+
+
+  describe('Github Authentication: ',function(){
+  	it('should redirect client to / if authentication fails', function(){
+
+  	});
+
+  	it('should redirect student client to /student.html if authentication succeeds', function(){
+
+  	});
+
+  	it('should redirect teacher client to /teacher.html if authentication succeeds', function(){
+
+  	});
+  });
+
+  describe('Account Creation: ', function(){
+    it('should create a new account for user logging in for the first time', function(){
+
+    });
+
+  });
+
+  describe('Account Logout: ', function(){
+  	it('should logout client', function(){
+
+  	});
+  });
+
+
+
+})
